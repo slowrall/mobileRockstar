0ae5fdeb387619a94bb4e2ca356a91be8918ad99
Oge Nwabuoku <oge.g.nwabuoku@accenture.com>
2015-04-15 17:09:52 -0700

    First set of files added to MobileRockstar

---
+++ mobileRockstar/mobileRockstar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mobileRockstar
+//
+//  Created by OgeNwabuoku on 4/15/15.
+//  Copyright (c) 2015 OgeNwabuoku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mobileRockstar/mobileRockstar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mobileRockstar
+//
+//  Created by OgeNwabuoku on 4/15/15.
+//  Copyright (c) 2015 OgeNwabuoku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
